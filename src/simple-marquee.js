a835b4ca471d32597137f23e6388dfe632993661
Maksim Smirnov <maxinflame@yandex.ru>
2024-04-14 17:16:56 +0300

    create copies

--- src/simple-marquee.js
+++ src/simple-marquee.js
@@ -2,13 +2,28 @@
 //   node: selector | DOM element
 // }
 
-import { getNode } from "./modules/utils/getNode";
+import { getNode } from "./modules/getNode";
+import { createCopies } from "./modules/createCopies";
 
 class SimpleMarquee {
   constructor(node, options) {
-    this.wrapper = getNode(node);
+    this._wrapper = getNode(node);
+    this._content = this._wrapper.querySelector('.simple-marquee-content');
+    this._copies = createCopies(this._wrapper, this._content);
+    this._init();
+  }
 
-    console.log(this.wrapper)
+  _init() {
+    this._wrapper.style.overflow = 'hidden';
+    this._wrapper.style.position = 'relative';
+    this._wrapper.style.width = '100%'
+    this._wrapper.style.height = `${this._wrapper.offsetHeight}px`;
+
+    this._wrapper.innerHTML = '';
+    this._copies.forEach(copy => {
+      this._wrapper.appendChild(copy.node)
+      copy.setOnPosition();
+    })
   }
 }
 
